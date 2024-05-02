bcc9d51c9dae143c35784d430f007969895f2993
Danylo77 <dankoiniesta@gmail.com>
2024-05-03 00:05:21 +0300

    Created Spring project

---
+++ TakeUrTableApp/src/test/java/com/example/takeurtableapp/TakeUrTableAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.takeurtableapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeUrTableAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
